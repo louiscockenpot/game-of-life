f33b1b83e59c3647c28dc47c4a7a311ce019e33a
louiscockenpot <louis.cockenpot@edu.devinci.fr>
2024-01-29 14:33:20 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+name='game-of-life',
+version='1.0',
+author='louiscockenpot',
+license='MIT',
+long_description=open('README.md').read(),
+)
