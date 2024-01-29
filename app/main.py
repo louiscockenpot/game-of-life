f07ad11cf156494bc46581030c00ba1f2b3dd534
louiscockenpot <louis.cockenpot@edu.devinci.fr>
2024-01-29 15:45:27 +0100

    Update number of voisins to kill a cell

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
+import threading
 import os
 import numpy as np
 import time
+
+# Global variable to control the game state
+game_paused = False
 
 def init_grille(taille):
     return np.random.choice(['  ', '# '], taille * taille, p=[0.8, 0.2]).reshape(taille, taille)
 
 
 def afficher_grille(grille):
+    os.system('cls' if os.name == 'nt' else 'clear')
     for i in range(grille.shape[0]):
         for j in range(grille.shape[1]):
             print(grille[i, j], end='')
-        print()        
+        print()
 
 
 def calculer_prochain_etat(grille):
@@ -24,7 +29,7 @@ def calculer_prochain_etat(grille):
                         continue
                     if grille[(i + k) % grille.shape[0], (j + l) % grille.shape[1]] == '# ':
                         nb_voisins += 1
-            if grille[i, j] == '# ' and (nb_voisins < 2 or nb_voisins > 3):
+            if grille[i, j] == '# ' and (nb_voisins < 2 or nb_voisins > 10):
                 nouvelle_grille[i, j] = '  '
             elif grille[i, j] == '  ' and nb_voisins == 3:
                 nouvelle_grille[i, j] = '# '
@@ -32,13 +37,29 @@ def calculer_prochain_etat(grille):
     return nouvelle_grille
 
 
+def handle_user_input():
+    global game_paused
+    while True:
+        input_char = input()
+        if input_char.lower() == 'p':
+            game_paused = not game_paused
+
+
 def jeu_de_la_vie(taille, generations):
+    global game_paused
     grille = init_grille(taille)
+
+    # Start the user input thread
+    input_thread = threading.Thread(target=handle_user_input)
+    input_thread.daemon = True
+    input_thread.start()
+
     for _ in range(generations):
+        while game_paused:
+            time.sleep(0.1)
         afficher_grille(grille)
         grille = calculer_prochain_etat(grille)
         time.sleep(0.3)
-        os.system('cls' if os.name == 'nt' else 'clear')
-        
+
 
 jeu_de_la_vie(40, 100)
